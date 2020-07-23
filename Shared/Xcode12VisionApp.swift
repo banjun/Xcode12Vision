a1a2b16f5926ba896acfd3d0ff517e93713d14b6
banjun <banjun@gmail.com>
2020-07-23 15:37:09 +0900

    Initial Commit

---
+++ Shared/Xcode12VisionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Xcode12VisionApp.swift
+//  Shared
+//
+//  Created by BAN Jun on 2020/07/23.
+//
+
+import SwiftUI
+
+@main
+struct Xcode12VisionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
